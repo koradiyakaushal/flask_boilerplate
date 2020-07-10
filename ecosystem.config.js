05df7dd392cf838a58827314cd26d1d33fc63157
kaushal Koradiya <kaushal@mobifyi.com>
2020-07-10 23:26:51 +0530

    Feat: added ecosystem and requirements

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps : [{
+      name: 'Flask-App',
+      script: 'pipenv run python',
+      args: 'api/run.py',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      interpreter:'/var/lib/jenkins/workspace/test 2/venv/bin/activate'
+    }]
+  };
+  
